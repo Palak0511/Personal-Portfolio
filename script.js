c11648b3128b76d3ff7e5fbf1760aa9ca78ea60f
Palak Sharma <144250708+Palak0511@users.noreply.github.com>
2024-09-14 20:30:23 +0530

    resume updated

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-document.querySelector(".cross").style.Display = "none";
+document.querySelector(".cross").style.display = "none";
+
 document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".sidebar").classList.toggle("sidebarGo");
-  if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
-    document.querySelector(".ham").style.display = "inline";
-    document.querySelector(".cross").style.display = "none";
+  const sidebar = document.querySelector(".sidebar");
+  const ham = document.querySelector(".ham");
+  const cross = document.querySelector(".cross");
+  
+  sidebar.classList.toggle("sidebarGo");
+
+  if (sidebar.classList.contains("sidebarGo")) {
+    ham.style.display = "inline";
+    cross.style.display = "none";
   } else {
-    document.querySelector(".ham").style.display = "none";
+    ham.style.display = "none";
     setTimeout(() => {
-      document.querySelector(".cross").style.display = "inline";
-    }, 350);
+      cross.style.display = "inline";
+    }, 300); // Adjust the timeout to match the CSS transition duration
   }
 });
+
